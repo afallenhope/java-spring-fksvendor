099e11b8879f7561eb4a83e856ca2859158530a2
Fallen Kiyori <1794152+afallenhope@users.noreply.github.com>
2024-08-25 15:56:53 -0400

    Seeding the database

--- src/main/java/lol/fallen/FKSVendor/FksVendorApplication.java
+++ src/main/java/lol/fallen/FKSVendor/FksVendorApplication.java
@@ -23,8 +23,19 @@
 
 package lol.fallen.FKSVendor;
 
+import lol.fallen.FKSVendor.models.UserRole;
+import lol.fallen.FKSVendor.models.VendorUser;
+import lol.fallen.FKSVendor.services.UserService;
+import lol.fallen.FKSVendor.services.VendorStoreService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.UUID;
 
 @SpringBootApplication
 public class FksVendorApplication {
@@ -33,4 +44,47 @@ public class FksVendorApplication {
         SpringApplication.run(FksVendorApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner run(UserService userService, VendorStoreService vendorStoreService) {
+        return args -> {
+            userService.saveRole(new UserRole(null, "ROLE_USER"));
+            userService.saveRole(new UserRole(null, "ROLE_MANAGER"));
+            userService.saveRole(new UserRole(null, "ROLE_BLOGGER"));
+            userService.saveRole(new UserRole(null, "ROLE_OWNER"));
+            userService.saveRole(new UserRole(null, "ROLE_TRUSTED"));
+            var bCryptEncoder = new BCryptPasswordEncoder();
+
+            userService.saveUser(
+                    new VendorUser(
+                            UUID.randomUUID(),
+                            UUID.fromString("e6e73511-8a2c-4a11-acf6-6adeba0b99f9"),
+                            "fallen",
+                            "shadow",
+                            "fallen.shadow",
+                            bCryptEncoder.encode("supersecret"),
+                            new HashSet<UserRole>(),
+                            new Date()
+                    )
+            );
+
+            userService.saveUser(
+                    new VendorUser(
+                            UUID.randomUUID(),
+                            UUID.fromString("e2dca752-503a-4629-83d2-d40278e20723"),
+                            "sullen",
+                            "nebula",
+                            "sullen.nebula",
+                            bCryptEncoder.encode("password"),
+                            new HashSet<UserRole>(),
+                            new Date()
+                    )
+            );
+
+            userService.addRoleToUser("fallen.shadow", "ROLE_OWNER");
+            userService.addRoleToUser("fallen.shadow", "ROLE_MANAGER");
+
+            userService.addRoleToUser("sullen.nebula", "ROLE_TRUSTED");
+            userService.addRoleToUser("sullen.nebula", "ROLE_MANAGER");
+        };
+    }
 }
