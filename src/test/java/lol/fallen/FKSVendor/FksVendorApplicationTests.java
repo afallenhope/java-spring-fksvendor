94ea220361b95e5a1aa5b16b8ae5f1495fc4fcad
Fallen Kiyori <1794152+afallenhope@users.noreply.github.com>
2024-08-26 04:33:33 -0400

    formatting

--- src/test/java/lol/fallen/FKSVendor/FksVendorApplicationTests.java
+++ src/test/java/lol/fallen/FKSVendor/FksVendorApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class FksVendorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
